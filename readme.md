35794c9ff7067089201db99a93f6fbc051a3fd0d
Slava Fomin II <s.fomin@betsol.ru>
2014-05-18 13:22:57 +0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,136 @@
+# angular-input-modified 1.0.0
+
+This AngularJS module adds additional properties and methods to the `ngModel` and `ngForm` controllers
+to provide end-user with facilities to detect and indicate form input modifications.
+
+This extra functionality allows you to provide better usability for forms.
+For example, you can add decorations to the form elements that are actually changed.
+That way, user will see what values has changed since last edit.
+
+Also, you can reset form to it's initial state (cancel all user edits) with just a single call to `form.reset()` or
+lock new values (preserve new state) just by calling `form.$setPristine()`.
+If you want, you can do this for individual input elements the same way.
+
+Please see [the Demo][demo].
+
+---
+
+## Decorations and animation
+
+This module adds `ng-modified` and `ng-not-modified` CSS classes to the input fields to indicate their state.
+Use them in your CSS to decorate input fields. You can combine multiple classes in the same selector.
+For example this is the useful CSS selector to decorate as valid only modified elements: `input.ng-valid.ng-modified`.
+This way target user will see what elements were actually changed.
+
+### Example
+
+``` css
+/** Decorating only modified inputs as valid */
+input.ng-valid.ng-modified {
+    border-color: green;
+}
+```
+
+It also supports animations if `ngAnimate` module is present.
+
+---
+
+## API
+
+### ngModel
+
+**masterValue** - 
+initial value of the input field.
+
+**modified** - 
+flag that indicates whether the input value was modified.
+
+**reset()** - 
+method to reset input value to it's initial state.
+
+### ngForm
+
+**modifiedCount** - 
+number of modified input types inside of the form.
+
+**modified** - 
+flag that indicates whether the form is modified (i.e. at least one element is modified).
+
+**reset()** - 
+method to reset all input values of the form to their initial states.
+
+---
+
+## Installation
+
+### Install library with bower
+
+`bower install --save angular-input-modified`
+
+### Add library to your page
+
+``` html
+<script type="text/javascript" src="angular-input-modified/src/angular-input-modified.js"></script>
+```
+
+### Add dependency in your application's module definition
+
+``` javascript
+var application = angular.module('application', [
+    // ...
+    'ngInputModified'
+]);
+```
+
+---
+
+## Usage
+
+Please see [the demo][demo] for all usage details. It has all the examples.
+
+---
+
+## Feedback
+
+If you have found a bug or have another issue with the library - please [create an issue][new-issue] in this GitHub repository.
+
+If you have a question - file it with [StackOverflow][so-ask] and send me a
+link to [s.fomin@betsol.ru][email]. I will be glad to help.
+Also, please add a [JSFiddle][jsfiddle] to demonstrate the issue if appropriate.
+You can even fork our [demo fiddle][demo].
+
+Have any ideas or propositions? Feel free to contact me by the same [E-Mail address][email].
+
+Cheers!
+
+---
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Slava Fomin II
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+[so-ask]: http://stackoverflow.com/questions/ask?tags=angularjs,javascript
+[email]: mailto:s.fomin@betsol.ru
+[jsfiddle]: http://jsfiddle.net/
+[demo]: http://jsfiddle.net/slavafomin/R6Yhx/
+[new-issue]: https://github.com/betsol/angular-input-modified/issues/new
---
+++ src/angular-input-modified.js
@@ -0,0 +1,194 @@
+(function(window, angular) {
+    'use strict';
+
+    var modifiedClassName = 'ng-modified';
+    var notModifiedClassName = 'ng-not-modified';
+
+    var inputModifiedDirectiveSpecification = ['$animate', inputModifiedDirective];
+
+    // Registering AngularJS module.
+    angular.module('ngInputModified', ['ng'])
+        .directive('input',    inputModifiedDirectiveSpecification)
+        .directive('textarea', inputModifiedDirectiveSpecification)
+        .directive('select',   inputModifiedDirectiveSpecification)
+    ;
+
+    function inputModifiedDirective($animate)
+    {
+        return {
+            restrict: 'E',
+            require: ['?ngModel', '?^form'],
+            link: function($scope, $element, attrs, controllers) {
+
+                // Handling controllers.
+                var ngModel = controllers[0];
+                var ngForm = controllers[1];
+
+                // ngModel is required for this directive to operate.
+                // ngForm is optional.
+                if (null === ngModel) {
+                    return;
+                }
+
+                // Saving handle to original $setPristine method.
+                var originalSetPristine = ngModel.$setPristine;
+
+                /**
+                 * This flag will show if input value was modified.
+                 *
+                 * @type {boolean}
+                 */
+                ngModel.modified = false;
+
+                /**
+                 * This property contains current master value for this input field.
+                 *
+                 * @type {undefined}
+                 */
+                ngModel.masterValue = undefined;
+
+                /**
+                 * Augmentation for original $setPristine method.
+                 */
+                ngModel.$setPristine = function() {
+                    // Calling original $setPristine method.
+                    originalSetPristine.apply(this, arguments);
+
+                    // Updating parameters.
+                    ngModel.masterValue = ngModel.$modelValue;
+                    ngModel.modified = false;
+
+                    // Re-decorating the element.
+                    toggleCssClasses();
+                };
+
+                /**
+                 * Resets input value to the master.
+                 */
+                ngModel.reset = function() {
+                    eval('$scope.' + attrs.ngModel + ' = ngModel.masterValue;');
+                };
+
+                // If parent form element is present for this input.
+                if (ngForm) {
+                    /**
+                     * Resets all form inputs to it's master values.
+                     */
+                    ngForm.reset = function() {
+                        angular.forEach(ngForm, function(element) {
+                            if (
+                                   'undefined' !== typeof element.$modelValue
+                                && 'function'  === typeof element.reset
+                            ) {
+                                element.reset();
+                            }
+                        });
+                    };
+
+                    ngForm.modified = false;
+                    ngForm.modifiedCount = 0;
+                }
+
+                /**
+                 * Decorates element with proper CSS classes.
+                 */
+                var toggleCssClasses = function()
+                {
+                    $animate.addClass($element, (ngModel.modified ? modifiedClassName : notModifiedClassName));
+                    $animate.removeClass($element, (ngModel.modified ? notModifiedClassName : modifiedClassName));
+                }
+
+                /**
+                 * Flag to determine if this is the first call.
+                 *
+                 * @type {boolean}
+                 */
+                var initial = true;
+
+                // Watching for model value changes.
+                $scope.$watch(attrs.ngModel, function(value) {
+
+                    if (ngModel.$name == 'text-required') {
+                        console.log('Value for: ' + ngModel.$name + ' changed to: "' + value + '"');
+                    }
+
+                    // If master value is not set.
+                    if (initial) {
+                        // Preserving master value.
+                        ngModel.masterValue = value;
+                        // Initially decorating the element.
+                        toggleCssClasses();
+
+                    } else {
+                        // Comparing current input value with preserved master value
+                        // to determine if it's changed.
+                        var modified = !valuesEqual(value, ngModel.masterValue);
+
+                        // If modified flag is changed.
+                        if (ngModel.modified !== modified) {
+
+                            if (ngForm) {
+                                ngForm.modifiedCount += (modified ? 1 : -1);
+                                ngForm.modified = (ngForm.modifiedCount > 0);
+                            }
+
+                            // Setting new flag.
+                            ngModel.modified = modified;
+                            // Re-decorating the element.
+                            toggleCssClasses();
+                        }
+                    }
+
+                    // Disabling initial flag in the end of first run.
+                    if (initial) {
+                        initial = false;
+                    }
+                });
+            }
+        };
+    }
+
+    /**
+     * Returns true if specified values are equal, false otherwise.
+     * Supports date comparison.
+     *
+     * @param {*} value1
+     * @param {*} value2
+     * @returns {boolean}
+     */
+    function valuesEqual(value1, value2)
+    {
+        value1 = unifyValue(value1);
+        value2 = unifyValue(value2);
+
+        if ('object' === typeof value1 && 'object' === typeof value2) {
+            if (value1 instanceof Date && value2 instanceof Date) {
+                // Comparing two dates.
+                return (value1.getTime() === value2.getTime());
+            }
+        }
+
+        // In all other cases.
+        return (value1 === value2);
+    }
+
+    /**
+     * Casting all null-like values to actual null for guaranteed comparison.
+     *
+     * @param {*} value
+     * @returns {*}
+     */
+    function unifyValue(value)
+    {
+        if (undefined === value) {
+            return null;
+        }
+
+        if ('' === value) {
+            return null;
+        }
+
+        return value;
+    }
+
+})(window, angular);

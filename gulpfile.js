000a50f2cfce8d277f6de730b04619cbc5672f55
Slava Fomin II <s.fomin@betsol.ru>
2015-04-09 19:04:31 +0300

    SPA server is now used instead of connect.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,49 @@
-var del     = require('del');
-var gulp    = require('gulp');
-var rename  = require('gulp-rename');
-var uglify  = require('gulp-uglify');
-var gutil   = require('gulp-util');
-var deploy  = require('gulp-gh-pages');
-var debug   = require('gulp-debug');
-var connect = require('gulp-connect');
+var del = require('del');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+var deploy = require('gulp-gh-pages');
+var debug = require('gulp-debug');
+var serverFactory = require('spa-server');
+
 
 gulp.task('clean', function (callback) {
-    del(['dist'], callback);
+  del(['dist'], callback);
 });
+
 
 gulp.task('build', ['clean'], function () {
-    gulp.src('src/angular-input-modified.js')
-        .pipe(rename('angular-input-modified.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(uglify())
-        .pipe(rename('angular-input-modified.min.js'))
-        .pipe(gulp.dest('dist'))
-        .on('error', gutil.log)
-    ;
+  gulp.src('src/angular-input-modified.js')
+    .pipe(rename('angular-input-modified.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(rename('angular-input-modified.min.js'))
+    .pipe(gulp.dest('dist'))
+    .on('error', gutil.log)
+  ;
 });
+
 
 gulp.task('deploy', function () {
-    return gulp.src('./demos/**/*.*')
-        .pipe(debug({
-            title: 'Deploy'
-        }))
-        .pipe(deploy({}));
+  return gulp.src('./demos/**/*.*')
+    .pipe(debug({
+      title: 'Deploy'
+    }))
+    .pipe(deploy());
 });
 
-gulp.task('webserver', function() {
-    connect.server({
-        root: './demos',
-        port: 8888
-    });
+
+gulp.task('webserver', function () {
+  var server = serverFactory.create({
+    path: './demos',
+    port: 8888,
+    serveStaticConfig: {
+      index: 'index.html'
+    }
+  });
+  server.start();
 });
+
 
 gulp.task('default', ['build']);
